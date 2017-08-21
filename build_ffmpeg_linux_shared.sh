c6161d753019fff3a80ac8b1d270c48d1f5fc8d1
shuh <shuh@localhost.localdomain>
2017-08-21 15:46:32 +0000

    完善脚本，增加libmp3lame依赖库

--- build_ffmpeg_linux_shared.sh
+++ build_ffmpeg_linux_shared.sh
@@ -3,24 +3,28 @@ echo "========================================="
 echo "=====build ffmpeg linux shared begin====="
 echo "========================================="
 
-ROOT_PATH=`pwd`/..
-RELEASE_PATH=/home/shuh/openlib
+ROOT_PATH=`pwd`
+RELEASE_PATH=$ROOT_PATH/output/linux/shared
 
-X264_RELEASE_PATH=$RELEASE_PATH/libx264
-X264_BUILD_PATH=$ROOT_PATH/ffmpeg/x264-20160920-72d53ab
+X264_RELEASE_PATH=$RELEASE_PATH
+X264_BUILD_PATH=$ROOT_PATH/x264-20170123-90a61ec
 
-X265_RELEASE_PATH=$RELEASE_PATH/libx265
-X265_BUILD_PATH=$ROOT_PATH/ffmpeg/x265-2.1/build/linux
+X265_RELEASE_PATH=$RELEASE_PATH
+X265_BUILD_PATH=$ROOT_PATH/x265-2.2/build/linux
+
+MP3LAME_RELEASE_PATH=$RELEASE_PATH
+MP3LAME_BUILD_PATH=$ROOT_PATH/lame-3.99.5
 
 FFMPEG_RELEASE_PATH=$RELEASE_PATH/ffmpeg
-FFMPEG_BUILD_PATH=$ROOT_PATH/ffmpeg
+FFMPEG_BUILD_PATH=$ROOT_PATH/tmp
 
 #cleaning release files
 rm -rf $RELEASE_PATH/*
 
-echo "=====build libx264 begin======"
-mkdir -p $X264_RELEASE_PATH
+chmod -R +x *
+mkdir -p $RELEASE_PATH
 
+echo "=====build libx264 begin======"
 cd $X264_BUILD_PATH
 ./configure --prefix=$X264_RELEASE_PATH --enable-shared
 make && make install
@@ -33,32 +37,42 @@ else
 fi
 
 echo "=====build libx265 begin====="
-mkdir -p $X265_RELEASE_PATH
-
-cd $X265_BUILD_PATH
-#cleaning having built files  
-rm -rf *
+cd $X265_BUILD_PATH && rm -rf *
 
 cmake -DCMAKE_INSTALL_PREFIX=$X265_RELEASE_PATH ../../source && cmake ../../source
 make && make install
 
-if [ -f "${X265_RELEASE_PATH}/lib/libx265.so.95" ]; then
+if [ -f "${X265_RELEASE_PATH}/lib/libx265.so.102" ]; then
 	cd -
 else
 	echo "=====build libx265 failed====="
 	exit 1
 fi
 
-echo "=====build ffmpeg begin====="
-export PKG_CONFIG_PATH=$X264_RELEASE_PATH/lib/pkgconfig:$X265_RELEASE_PATH/lib/pkgconfig
+echo "=====build libmp3lame begin====="
+cd $MP3LAME_BUILD_PATH
+./configure --prefix=$MP3LAME_RELEASE_PATH --enable-shared
+make && make install
 
-if [ -d $FFMPEG_BUILD_PATH/tmp ];then 
-	rm -r $FFMPEG_BUILD_PATH/tmp
+if [ -f "${MP3LAME_RELEASE_PATH}/lib/libmp3lame.so.0" ]; then
+	cd -
+else
+	echo "=====build libmp3lame failed====="
+	exit 1
 fi
-mkdir $FFMPEG_BUILD_PATH/tmp
 
-cd $FFMPEG_BUILD_PATH/tmp
-../ffmpeg-3.2.1/configure --prefix=$FFMPEG_RELEASE_PATH --enable-shared --enable-gpl --enable-libx264 --enable-libx265
+
+echo "=====build ffmpeg begin====="
+export PKG_CONFIG_PATH=$X265_RELEASE_PATH/lib/pkgconfig:$PKG_CONFIG_PATH
+mkdir -p $FFMPEG_RELEASE_PATH
+
+if [ -d $FFMPEG_BUILD_PATH ];then 
+	rm -rf $FFMPEG_BUILD_PATH
+fi
+
+mkdir -p $FFMPEG_BUILD_PATH
+cd $FFMPEG_BUILD_PATH
+./../FFmpeg-release-3.1/configure --prefix=$FFMPEG_RELEASE_PATH --enable-shared --disable-ffserver --disable-ffprobe --enable-gpl --enable-libx264 --enable-libx265 --enable-libmp3lame --extra-cflags=-I$RELEASE_PATH/include --extra-ldflags="-L$RELEASE_PATH/lib -lx264 -lpthread -lm -ldl -lmp3lame" --extra-libs=-lstdc++
 make && make install
 
 if [ -f "${FFMPEG_RELEASE_PATH}/bin/ffmpeg" ]; then
--- build_ffmpeg_linux_static.sh
+++ build_ffmpeg_linux_static.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 echo "========================================="
-echo "=====build ffmpeg linux shared begin====="
+echo "=====build ffmpeg linux static begin====="
 echo "========================================="
 
 ROOT_PATH=`pwd`
-RELEASE_PATH=$ROOT_PATH/output
+RELEASE_PATH=$ROOT_PATH/output/linux/static
 
-X264_RELEASE_PATH=$RELEASE_PATH/libx264
+X264_RELEASE_PATH=$RELEASE_PATH
 X264_BUILD_PATH=$ROOT_PATH/x264-20170123-90a61ec
 
-X265_RELEASE_PATH=$RELEASE_PATH/libx265
+X265_RELEASE_PATH=$RELEASE_PATH
 X265_BUILD_PATH=$ROOT_PATH/x265-2.2/build/linux
 
+MP3LAME_RELEASE_PATH=$RELEASE_PATH
+MP3LAME_BUILD_PATH=$ROOT_PATH/lame-3.99.5
+
 FFMPEG_RELEASE_PATH=$RELEASE_PATH/ffmpeg
-FFMPEG_BUILD_PATH=$ROOT_PATH/FFmpeg-release-3.1
+FFMPEG_BUILD_PATH=$ROOT_PATH/tmp
+
+
+#cleaning release files
+rm -rf $RELEASE_PATH/*
 
 chmod -R +x *
+mkdir -p $RELEASE_PATH
 
 echo "=====build libx264 begin======"
-mkdir -p $X264_RELEASE_PATH
-
 cd $X264_BUILD_PATH
 ./configure --prefix=$X264_RELEASE_PATH --enable-static
 make && make install
@@ -32,9 +38,8 @@ else
 fi
 
 echo "=====build libx265 begin====="
-mkdir -p $X265_RELEASE_PATH
+cd $X265_BUILD_PATH && rm -rf *
 
-cd $X265_BUILD_PATH
 cmake -DCMAKE_INSTALL_PREFIX=$X265_RELEASE_PATH -DENABLE_SHARED=no ../../source && cmake ../../source
 make && make install
 
@@ -45,12 +50,31 @@ else
 	exit 1
 fi
 
+echo "=====build libmp3lame begin====="
+cd $MP3LAME_BUILD_PATH
+./configure --prefix=$MP3LAME_RELEASE_PATH --enable-static
+make && make install
+
+if [ -f "${MP3LAME_RELEASE_PATH}/lib/libmp3lame.a" ]; then
+	rm -f ${MP3LAME_RELEASE_PATH}/lib/libmp3lame.so*
+	cd -
+else
+	echo "=====build libmp3lame failed====="
+	exit 1
+fi
+
+
 echo "=====build ffmpeg begin====="
-export PKG_CONFIG_PATH=$X264_RELEASE_PATH/lib/pkgconfig:$X265_RELEASE_PATH/lib/pkgconfig
+export PKG_CONFIG_PATH=$X265_RELEASE_PATH/lib/pkgconfig:$PKG_CONFIG_PATH
 mkdir -p $FFMPEG_RELEASE_PATH
 
+if [ -d $FFMPEG_BUILD_PATH ];then 
+	rm -rf $FFMPEG_BUILD_PATH
+fi
+
+mkdir -p $FFMPEG_BUILD_PATH
 cd $FFMPEG_BUILD_PATH
-./configure --prefix=$FFMPEG_RELEASE_PATH --enable-static --enable-gpl --enable-libx264 --enable-libx265 --extra-libs='-lstdc++'
+./../FFmpeg-release-3.1/configure --prefix=$FFMPEG_RELEASE_PATH --enable-static --disable-ffserver --disable-ffprobe --enable-gpl --enable-libx264 --enable-libx265 --enable-libmp3lame --extra-cflags=-I$RELEASE_PATH/include --extra-ldflags="-L$RELEASE_PATH/lib -lx264 -lpthread -lm -ldl -lmp3lame -lstdc++"
 make && make install
 
 if [ -f "${FFMPEG_RELEASE_PATH}/bin/ffmpeg" ]; then
@@ -61,7 +85,7 @@ else
 fi
 
 echo "==========================================="
-echo "=====build ffmpeg linux shared success====="
+echo "=====build ffmpeg linux static success====="
 echo "==========================================="
 
 
